4f28f6e19369774491c1bd2aa525cb75ac2985c4
nabilazman684-eng <nabilazman684@gmail.com>
2026-09-16 23:34:19 +0800

    Add files via upload

---
+++ drink.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pak Abu Stall</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<header>
+  <h1>Selamat Datang</h1>
+  <p><b>Rasa Lapar? Pak Abu kan ada!</b></p>
+</header>
+
+<div class="navbar">
+  <div class = "page">
+    <a href="homepage.php"><i class="bi bi-house"></i></a>
+    <a href="drink.php"><i class="bi bi-cup-straw"></i>drink</a>
+    <a href="food.php"><i class="bi bi-fork-knife"></i>food</a>
+    <a href="About Us"><i class="bi bi-info-circle"></i>About Us</a>
+  </div>
+  <div class="cart">
+    <i class="bi bi-cart"></i>
+    <div class="cart-count">0</div>
+  </div>
+</div>
+
+<div class="shop" id="shop">
+  <div class="item">
+        <img width="220" src="images/img-2.jpg" alt="">
+        <div class="details">
+            <h3>Sate Bakar</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div> 
+      <div class="item">
+        <img width="220" src="images/img-2.jpg" alt="">
+        <div class="details">
+            <h3>Sate Bakar</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div> 
+      <div class="item">
+        <img width="220" src="images/img-2.jpg" alt="">
+        <div class="details">
+            <h3>Sate Bakar</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div> 
+      <div class="item">
+        <img width="220" src="images/img-2.jpg" alt="">
+        <div class="details">
+            <h3>Sate Bakar</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div> 
+      <div class="item">
+        <img width="220" src="images/img-2.jpg" alt="">
+        <div class="details">
+            <h3>Sate Bakar</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div> 
+      <div class="item">
+        <img width="220" src="images/img-2.jpg" alt="">
+        <div class="details">
+            <h3>Sate Bakar</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                <i class="bi bi-dash-lg"></i>
+                <div class="quantity">0</div>
+                <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
+
+<script src="main.js"></script>
+<!-- Footer -->
+<footer>
+  <h2>&copy; Nabil Azman</h2>
+</footer>
+
+</body>
+</html>
